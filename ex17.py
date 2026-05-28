cf166d622273bca4a3c6a75f93647771d8fe6946
GVC9 <gabrielvieiracoelho8@gmail.com>
2026-05-27 21:10:50 -0300

    "ex17.py"

---
+++ ex17.py
@@ -0,0 +1,7 @@
+temp=float(input("Digite a temperatura atual:"))
+if temp < 18:
+    print(f"A temperatura {temp} é fria.")
+elif temp >= 18 and temp <= 30:
+    print(f"A temperatura {temp} é agradável.")
+elif temp > 30: 
+ print (f"A temperatura {temp} é calor.") 
